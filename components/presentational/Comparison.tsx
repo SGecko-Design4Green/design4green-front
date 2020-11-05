451d233e4e9cf7ed7517559909d17349f4b9e458
Jean-Baptiste WATENBERG <jeanbaptiste.watenberg@gmail.com>
2020-11-05 11:25:22 +0100

    stuffs

---
+++ components/presentational/Comparison.tsx
@@ -0,0 +1,28 @@
+const getPercent = (current: number, reference: number): number =>
+    Math.round(((100 * current / reference) - 100) * 100) / 100;
+
+const GREEN = '#03bd5b';
+const RED = '#d63626';
+
+const Percent = ({ percent }: { percent: number }) => {
+    const color = percent > 0 ? GREEN : RED;
+    const prefix = percent > 0 ? '+' : '';
+
+    return <span style={{ color }}>{prefix}{percent}%</span>;
+}
+
+export interface ComparisonProps {
+    readonly reference: number;
+    readonly label: string;
+    readonly value: number;
+}
+
+const Comparison = ({ label, value, reference }: ComparisonProps) => {
+
+    const percent = getPercent(value, reference);
+
+    return <><span>VS {label} {reference} (<Percent percent={percent} />)</span><br /></>;
+}
+    
+
+export default Comparison;
